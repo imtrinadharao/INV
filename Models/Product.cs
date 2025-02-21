26f0a754c32e0db27ba1b964a2c6d95fdc2921fc
imtrinadharao <imtrinadharao@gmail.com>
2025-02-21 15:16:11 +0530

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,9 +5,11 @@ namespace CRUDoperations.Models
     public class Product
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } "";
+
        
-        public string Category { get; set; }
+        public string Category { get; set; } "";
+
         
         public decimal Salary { get; set; }
         public DateTime CreatedAt { get; set; }
